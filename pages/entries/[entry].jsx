a9282e359ddf5037f0914ea03341a6e04cf017de
forrestdevelops <fsmietanski@gmail.com>
2023-03-12 13:46:47 -0400

    Added Routing and styling support

--- pages/entries/[entry].jsx
+++ pages/entries/[entry].jsx
@@ -1,10 +1,15 @@
+import BreadCrumb from "components/BreadCrumb"
+import Link from "next/link"
 import { getEntry } from "services/entries"
 
 export default function entry(props) {
     return (
-        <div className='entries' key={props.entry_data.entry_id}>
-            <h3>{props.entry_data.name}</h3>
-            <p>{props.entry_data.data}</p>
+        <div>
+            <BreadCrumb name={props.entry_data.name} />
+            <div className='entries' key={props.entry_data.entry_id}>
+                <h3>{props.entry_data.name}</h3>
+                <p>{props.entry_data.data}</p>
+            </div>
         </div>)
 }
 
