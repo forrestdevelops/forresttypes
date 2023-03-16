a52d214be9282870014ccd11100525848daaddf2
forrestdevelops <fsmietanski@gmail.com>
2023-03-16 07:54:51 -0400

    Added buy me coffee widget

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,8 +3,10 @@ export default function Footer() {
 
     return (
         <div className='footerBase'>
+            <div></div>
             <div className='footerContent'>
                 Forrest Smietanski &copy; {currentYear}
             </div>
+            <div><script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="fsmietanski" data-description="Support me on Buy me a coffee!" data-message="" data-color="#f1f3fb" data-position="Right" data-x_margin="18" data-y_margin="18"></script></div>
         </div>)
 }
